ffe0375cf88916e352c91f8c285f9a06aa2c0a6f
lfabio552 <lfabio552@gmail.com>
2025-11-21 16:45:40 -0300

    Fix: Corrige link de pagamento para Producao

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -71,7 +71,7 @@ export default function HomePage() {
     
     try {
       // ATENÇÃO: Estamos usando localhost para testar. Mude para o Render no futuro.
-      const response = await fetch('http://localhost:5000/create-checkout-session', { 
+      const response = await fetch('https://meu-gerador-backend.onrender.com/create-checkout-session', { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
